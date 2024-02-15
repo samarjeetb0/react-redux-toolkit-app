0c50d6c96570dec13404a8ffa22e3361b606e599
samar <samarjeetb0@gmail.com>
2024-02-15 13:40:46 +0530

    resdme update

--- src/store/Index.js
+++ src/store/Index.js
@@ -2,8 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import  counterSlice from './Counter';
 import  privacySlice from './Privacy';
 
-
-
 const counterStore = configureStore({
   reducer: {
     counter: counterSlice.reducer,
